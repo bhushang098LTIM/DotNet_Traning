e0a28eef48f6a9c2633a949c4c8f2126aaf974c3
bhushang098LTIM <bhushan.gurnule@mindtree.com>
2022-12-29 15:53:52 +0000

    Added Anonymus Type Pratical

--- Anonymus Type/Program.cs
+++ Anonymus Type/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var person = new { Name = "Bhushan", Age = 23 }; //Anonymus type Created
+
+// person.Name = "kartik";  Not Allowed because Anonymus type  Create only Read onnly Peoperties 
+
+Console.WriteLine(" name : {0} \n Age : {1}", person.Name,person.Age);
+
+var p = person; // Anonymus type are Reference type That Are Derived from Object Type
+
+Console.WriteLine(" name : {0} \n Age : {1}", p.Name, p.Age);
+
+var per = new { Name = "kartik", Age = 24 };
+
+if(p.Equals(per))
+{
+    Console.WriteLine("Equal objects");
+}
+else
+{
+    Console.WriteLine("Non-Equal objects");
+}
+
+if (object.ReferenceEquals(p,per))
+{
+    Console.WriteLine("Referenece Equal objects");
+}
+else
+{
+    Console.WriteLine("Refernece Non-Equal objects");
+}
+
